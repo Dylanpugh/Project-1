785f9f67607b884f6a8f478bca9a9dc5ba51799c
Dylan Pugh <pughdylan93@gmail.com>
2019-01-15 21:32:26 -0500

    modal/css/js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,7 @@ var settings = {
         for (var i=0; i < response.results.length; i++) {
           var appendrow = $("<div class='ui centered card'></div>").appendTo("#movie-container");
             $("<div class='image'><img src='https://image.tmdb.org/t/p/w500/" + response.results[i].poster_path  + 
-            "'><br><h4>"+response.results[i].title+" Release Date: "+response.results[i].release_date+"</h4><button type='button' class='ui red basic button' data-toggle='modal' data-src='"+response.results[i].title+"' data-target='#myModal'>Play Trailer</button></div>").appendTo(appendrow);
+            "'><br><h4>"+response.results[i].title+" Release Date: "+response.results[i].release_date+"</h4><button type='button' class='ui red basic button video-btn' data-toggle='modal' data-src='"+response.results[i].title+"' data-target='#myModal'>Play Trailer</button></div>").appendTo(appendrow);
         }
         for (var j=1;j<=response.total_pages;j++) {
             var append_pagination = $('<li class="page-item"><a class="page-link" data-page="'+j+'"href="#">'+j+'</a></li>').appendTo($(".pagination"));
@@ -75,7 +75,7 @@ var settings = {
         for (var i=0; i < response.results.length; i++) {
           var appendrow = $("<div class='ui centered card'></div>").appendTo("#movie-container");
             $("<div class='image'><img src='https://image.tmdb.org/t/p/w500/" + response.results[i].poster_path  + 
-            "'><br><h4>"+response.results[i].title+" Release Date: "+response.results[i].release_date+"</h4><button type='button' class='ui red basic button' data-toggle='modal' data-src='"+response.results[i].title+"' data-target='#myModal'>Play Trailer</button></div>").appendTo(appendrow);
+            "'><br><h4>"+response.results[i].title+" Release Date: "+response.results[i].release_date+"</h4><button type='button' class='ui red basic button video-btn' data-toggle='modal' data-src='"+response.results[i].title+"' data-target='#myModal'>Play Trailer</button></div>").appendTo(appendrow);
         }
         for (var j=1;j<=response.total_pages;j++) {
             var append_pagination = $('<li class="page-item"><a class="page-link" data-page="'+j+'"href="#">'+j+'</a></li>').appendTo($(".pagination"));
@@ -86,26 +86,21 @@ var settings = {
   
   });
   
-// Get the modal
 var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = $(".video-btn");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+ 
+$(document).on("click",btn,function() {
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
   modal.style.display = "block";
-}
+  });
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
