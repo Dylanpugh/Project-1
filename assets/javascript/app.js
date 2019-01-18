92ec94c4e896bf09b156c7b5d77e95908610fd9b
unknown <chuckgriffin1987@gmail.com>
2019-01-17 21:01:58 -0500

    uploading

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
-
-//movie api
+var url = 'https://api.themoviedb.org/3/discover/movie?primary_release_year=2019&page=1&api_key=6f0a2ab68362abacd4411d8533c75937';
+$( document ).ready(function() {
 var settings = {
     "async": true,
     "crossDomain": true,
@@ -8,25 +8,24 @@ var settings = {
     "headers": {},
     "data": "{}"
   }
-  //calling api
+  
   $.ajax(settings).done(function (response) {
     console.log(response.results);
   
-  //append to html
+  
         for (var i=0; i < response.results.length; i++) {
           var appendrow = $("<div class='ui centered card'></div>").appendTo("#movie-container");
             $("<div class='image'><img src='https://image.tmdb.org/t/p/w500/" + response.results[i].poster_path  + 
-            "'><br><h4 class='content'>"+response.results[i].title+" Release Date: "+response.results[i].release_date+"</h4><button type='button' class='ui yellow basic button video-btn' data-toggle='modal' data-src='"+response.results[i].title+"' data-target='#myModal'>Play Trailer</button></div>").appendTo(appendrow);
+            "'><br><h4>"+response.results[i].title+" Release Date: "+response.results[i].release_date+"</h4><button type='button' class='ui red basic button video-btn' data-toggle='modal' data-src='"+response.results[i].title+"' data-target='#myModal'>Play Trailer</button></div>").appendTo(appendrow);
         }
-        //create pagination
         for (var j=1;j<=response.total_pages;j++) {
             var append_pagination = $('<li class="page-item"><a class="page-link" data-page="'+j+'"href="#">'+j+'</a></li>').appendTo($(".pagination"));
         
         }
       });
   // ---------------------------------------------------------
+  });
   
-  //onclick function for youtube api
   var videoSrc;  
   $(document).on("click",".video-btn",function() {
       videoSrc = $(this).data( "src" );
@@ -44,59 +43,86 @@ var settings = {
       $.getJSON(url, params, showResults);
   }
   
-  //displays youtube trailor
   function showResults(results) {
       var entries = results.items[0].id.videoId;
       console.log(entries);
       $("#video").attr('src', "https://www.youtube.com/embed/"+entries+"?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1" );
   }
   
-  
-  
-   
-  
-  
   });
   
-  //when user clicks page number, follows command
+  
   $(document).on("click",".page-link",function() {
     page_number = $(this).attr('data-page');
   
     var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://api.themoviedb.org/3/discover/movie?primary_release_year=2019&page="+page_number+"&api_key=6f0a2ab68362abacd4411d8533c75937",
+    "url": "https://api.themoviedb.org/3/discover/movie?primary_release_year=2019&&api_key=6f0a2ab68362abacd4411d8533c75937&page="+page_number,
     "method": "GET",
     "headers": {},
     "data": "{}"
   }
   
-  //renders page clicked
   $.ajax(settings).done(function (response) {
     console.log(response.results);
-  
+  $( "#movie-container" ).empty();
     
         for (var i=0; i < response.results.length; i++) {
           var appendrow = $("<div class='ui centered card'></div>").appendTo("#movie-container");
             $("<div class='image'><img src='https://image.tmdb.org/t/p/w500/" + response.results[i].poster_path  + 
-            "'><br><h4 class='content'>"+response.results[i].title+" Release Date: "+response.results[i].release_date+"</h4><button type='button' class='ui yellow basic button video-btn' data-toggle='modal' data-src='"+response.results[i].title+"' data-target='#myModal'>Play Trailer</button></div>").appendTo(appendrow);
+            "'><br><h4>"+response.results[i].title+" Release Date: "+response.results[i].release_date+"</h4><button type='button' class='ui red basic button video-btn' data-toggle='modal' data-src='"+response.results[i].title+"' data-target='#myModal'>Play Trailer</button></div>").appendTo(appendrow);
         }
-        for (var j=1;j<=response.total_pages;j++) {
-            var append_pagination = $('<li class="page-item"><a class="page-link" data-page="'+j+'"href="#">'+j+'</a></li>').appendTo($(".pagination"));
         
-        }
       });
   
   
   });
-  //create variables for modal
+  
+  // Genres
+  $(document).on("click",".genre",function() {
+    genre = $(this).attr('data-genre');
+    var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api.themoviedb.org/3/discover/movie?with_genres="+genre+"&primary_release_year=2019&api_key=6f0a2ab68362abacd4411d8533c75937",
+    "method": "GET",
+    "headers": {},
+    "data": "{}"
+  }
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response.results);
+  $( "#movie-container" ).empty();
+    $( ".pagination" ).empty();
+        for (var i=0; i < response.results.length; i++) {
+          var appendrow = $("<div class='ui centered card'></div>").appendTo("#movie-container");
+            $("<div class='image'><img src='https://image.tmdb.org/t/p/w500/" + response.results[i].poster_path  + 
+            "'><br><h4>"+response.results[i].title+" Release Date: "+response.results[i].release_date+"</h4><button type='button' class='ui red basic button video-btn' data-toggle='modal' data-src='"+response.results[i].title+"' data-target='#myModal'>Play Trailer</button></div>").appendTo(appendrow);
+        }
+		for (var j=1;j<=response.total_pages;j++) {
+            var append_pagination = $('<li class="page-item"><a class="page-link" data-page="'+j+'"href="#">'+j+'</a></li>').appendTo($(".pagination"));
+        
+        }
+        
+      });
+  
+  
+  });
+  
+  
+  
+  
+  
+  
+  
 var modal = document.getElementById('myModal');
 
 var btn = $(".video-btn");
 
 var span = document.getElementsByClassName("close")[0];
- //onclick function to activate modal
-$(document).on("click",btn,function() {
+ 
+$(document).on("click",".video-btn",function() {
 
   modal.style.display = "block";
   });
@@ -110,4 +136,9 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+$('.ui.dropdown')
+  .dropdown()
+;
 	  
